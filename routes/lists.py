b57f26e487247b9fb4ccf9282c478d02413d0ace
Matthew Bisgaard <mbisgaard@my.bcit.ca>
2025-05-02 10:34:50 -0700

    Added: - The post route for the add item form - The get route for the add item form
    Changed:
    - The add item form html now has a method and url for the form so it can function
    - The todolist page now has has a link using url.for so the button can link to the page

--- routes/lists.py
+++ routes/lists.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from models import *
 from db import db
+from datetime import datetime as dt
 
 list_route = Blueprint("lists", __name__)
 
@@ -14,3 +15,33 @@ def get_list(id):
         return f"<h1>404</h1><br><h2>List {id} not found</h2>", 404
     
     return render_template("todolist.html", list=current_list)
+
+@list_route.route("/<int:id>/add", methods=["GET"])
+def get_add_item_form(id):
+    session = db.session
+    current_list = session.execute(db.select(List).where(List.id == id)).scalar()
+    if current_list is None: # check if the list exists and return a 404 error if it does not
+        return f"<h1>404</h1><br><h2>List {id} not found</h2>", 404
+    
+    return render_template("add-item-form.html", list=current_list)
+
+
+@list_route.route("/<int:id>/add", methods=["POST"])
+def add_reminder_to_list(id):
+    """ Adds a reminder from the add-item-form to the list """
+    session = db.session
+    current_list = session.execute(db.select(List).where(List.id == id)).scalar()
+    if current_list is None: # check if the list exists and return a 404 error if it does not
+        return f"<h1>404</h1><br><h2>List {id} not found</h2>", 404
+    
+
+    form = request.form
+    new_reminder = Todo(
+        title = form["item-name"],
+        description = form["description"],
+        deadline = dt.strptime(form["deadline"], "%Y-%m-%d"),
+        rem_list = current_list
+    )
+    session.add(new_reminder)
+    session.commit()
+    return redirect(url_for("lists.get_list", id=id))
