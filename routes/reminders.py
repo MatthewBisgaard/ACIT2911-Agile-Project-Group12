533f4017029c8b4d1ee22ffa66aa450618fc9304
Matthew Bisgaard <mbisgaard@my.bcit.ca>
2025-05-07 08:47:19 -0700

    Added the route to undo reminder completion

--- routes/reminders.py
+++ routes/reminders.py
@@ -28,6 +28,24 @@ def mark_reminder_completed_route(id):
 
     return redirect(url_for("lists.get_list", id=reminder.rem_list.id))
 
-
-
+@reminders_route.route("/de-complete/<int:id>", methods=["POST", "PUT"])
+def mark_reminder_incomplete_route(id):
+    """
+    Marks an already completed reminder as incomplete again.\n
+    This removes the completion date
+    """
+    reminder = db.session.execute(db.select(Todo).where(Todo.id == id)).scalar()
+    if reminder is None: # test that the reminder exists
+        return "Error 404: Reminder could not be found", 404
     
+    if not reminder.complete: # check reminder already complete
+        return "Error 409: Reminder not currently completed", 409
+    
+    reminder.complete = False
+    reminder.completed_on = None
+
+    # Update status in the db
+    db.session.add(reminder)
+    db.session.commit()
+
+    return redirect(url_for("lists.get_list", id=reminder.rem_list.id))
