c2bda1800461fa68b52a784b004a29d43269f6db
Matthew Bisgaard <mbisgaard@my.bcit.ca>
2025-05-14 13:02:59 -0700

    Finish the show/hide completed feature

--- static/js/queryUpdate.js
+++ static/js/queryUpdate.js
@@ -1,26 +1,31 @@
-// Found on https://stackoverflow.com/questions/5999118/how-can-i-add-or-update-a-query-string-parameter
-// Too good not to sue (with a few changes)
+// Originally ound on https://stackoverflow.com/questions/5999118/how-can-i-add-or-update-a-query-string-parameter
+// I have made many tweaks to better serve out purposes
 const updateQueryStringParameter = (uri, key, value) => {
   const re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
   const separator = uri.indexOf('?') !== -1 ? "&" : "?";
-  if (uri.match(re)) {
+
+
+  if (value === undefined){ // new clears if value is undefined
+    return uri.replace(re, '$1' + "$2");
+  } else if (uri.match(re)) {
     return uri.replace(re, '$1' + key + "=" + value + '$2');
-  }
-  else {
+  } else {
     return uri + separator + key + "=" + value;
   }
 }
 
-const toggleCompleteShow = () => {
-  
-}
-
 const load_url = location.toString()
 
-// Call this
+// Wrapper to immediately update the URL
 const updateURL = (key, value) => location = updateQueryStringParameter(load_url, key, value);
+
+// Toggles showing complete
+const toggleCompleteShow = () => {
+  updateURL("show", (load_url.includes("show=true")? "false":"true"))
+}
+
+// Makes the show/hide completed button look correct
 window.addEventListener('load', () => {
-  if (load_url.includes("show=true")){
-    document.getElementById
-  }
+  const toggleButton = document.getElementById("toggle-completed");
+  toggleButton.innerText = (load_url.includes("show=true")? "Hide":"Show")+" Completed";
 })
