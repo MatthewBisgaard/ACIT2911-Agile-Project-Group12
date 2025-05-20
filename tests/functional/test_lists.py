58c5cc010256da176be73eb7a35e5a36f3a08815
Matthew Bisgaard <mbisgaard@my.bcit.ca>
2025-05-20 12:17:39 -0700

    Added tests for 404 codes in items that do not exist. Also added existance check to the rename and delete routes

--- tests/functional/test_lists.py
+++ tests/functional/test_lists.py
@@ -11,26 +11,60 @@ def client():
     """ Testing client for functions regarding completion """
     app.config["TESTING"] = True
     with app.test_client() as client:
+        client.post("/auth/login", data={"username":"tom", "hashpasswd": "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"})
         yield client
 
-def add_test_todo(client):
+def add_test_list(client):
     """
     Test utility function:\n
     Adds a test todo via the item and returns uuid coresponding to the title of the test todo
     """
     unique_name = str(uuid.uuid4())
-    res = client.post("/lists/1/add", data={
-        "item-name": unique_name, 
-        "description": unique_name, 
-        "deadline":"2029-03-09T11:32"
+    res = client.post("/lists/create_list/completion", data={
+        "list-name": unique_name 
     })
     return unique_name
 
-def fetch_item_by_uuid(uuid_string):
+def fetch_list_by_uuid(uuid_string):
     """
     Test Utility function:\n
     Grabs a todo item directly from db by its title (a UUID for tests writted by Matthew)
     """
     with app.app_context():
-        todo = db.session.execute(db.select(Todo).where(Todo.title == uuid_string)).scalar()
-    return todo
+        remlist = db.session.execute(db.select(List).where(List.name == uuid_string)).scalar()
+    return remlist
+
+# Test for 404s
+def test_all_list_will_return_404(client):
+    """ Tests to make sure a list that does not exist returns a 404 error """
+    list_name = add_test_list(client)
+    list_id = fetch_list_by_uuid(list_name).id
+    with app.app_context():
+        remlist = db.session.execute(db.select(List).where(List.id == list_id)).scalar()
+        db.session.delete(remlist)
+        db.session.commit()
+
+    res = client.get(f"/lists/{list_id}")
+    assert res.status_code == 404 # Test you cannot get a list which doe snot exist
+
+    res = client.get(f"/lists/{list_id}/add")
+    assert res.status_code == 404 # SHould have a 404 for gettng the add item form for a list that does not exist
+
+    res = client.post(f"/lists/{list_id}/add")
+    assert res.status_code == 404 # 404 for adding an item to a list that does not exist 
+
+    res = client.get(f"/lists/rename_list/{list_id}")
+    assert res.status_code == 404 # 404 for getting the rename form for a list that does not exist
+
+    res = client.post(f"/lists/rename_list/{list_id}/completion")
+    assert res.status_code == 404 # 404 for submitting the rename form for a list that does not exist
+
+    res = client.get(f"/lists/delete_list/{list_id}")
+    assert res.status_code == 404 # 404 for getting the delete form for a list that does not exist
+
+    res = client.post(f"/lists/delete_list/{list_id}/completion")
+    assert res.status_code == 404 # 404 for submitting the delete form for a list that does not exist
+
+    res = client.get(f"/lists/{list_id}/delete-complete")
+    assert res.status_code == 404 # Check that a 404 is given when deleting all on a list that does not exist. Only one check needed due to the logic order in the function
+    
--- tests/functional/test_login.py
+++ tests/functional/test_login.py
@@ -23,8 +23,7 @@ def new_user_data():
 
 
 def test_handle_missing_values(client):
-    """ Tests to see that the fucntion handles missing form values\n
-NOTE: Once flash messages implemented on the forntend uncomment the commented assertations """
+    """ Tests to see that the fucntion handles missing form values """
     # Test missing username
     res = client.post("/auth/login", data={"hashpasswd": "1234"})
     assert res.status_code == 302 # Test redirect
@@ -63,3 +62,33 @@ def test_get_signin_page(client):
     """ Test that a get request to the route will get the login page (Looks at title)"""
     res = client.get("/auth/login")
     assert b"<title>Log In</title>" in res.data
+
+def test_signin_with_user_that_does_not_exist(client):
+    """ Tries to sign in with a user that does not exist. Checks for a redirect and the expected error message"""
+    res = client.post("/auth/login", data={"username": "Richard Hammond", "hashpasswd": "BlitheringIdiot"})
+    assert res.status_code == 302 # Check redirect
+    
+    res = client.get("/auth/login")
+    assert b"Username or password may be correct" in res.data # Check that the correct error message pops up
+
+def test_signin_with_bad_password(client):
+    """ Checks that the message for signing into an existing user with a bad password does not work """
+    res = client.post("/auth/login", data={"username": "tom", "hashpasswd": "KHANNNNN"})
+    assert res.status_code == 302 # Redirects back to login
+
+    res = client.get("/auth/login")
+    assert b"Username or password may be correct" in res.data # Check that the correct error message pops up
+    
+def test_redirect_with_next(client, new_user_data):
+    """ Tests to make sure the url in the next query string is what the client gets redirected to after sign in """
+    hash = hashlib.sha256(new_user_data["hashpasswd"].encode())
+    hash.update(b"salt")
+    db_version_password_hash = hash.hexdigest()
+
+    with app.app_context():
+        new_user = User(name=new_user_data["name"], username = new_user_data["username"], password=db_version_password_hash, salt = "salt")
+        db.session.add(new_user)
+        db.session.commit()
+
+    res = client.post("/auth/login?next=%2Fdashboard", data=new_user_data, follow_redirects=True)
+    assert res.request.path == "/dashboard" # Check that the new user is redirected to the dahsboard as the next param indicates
